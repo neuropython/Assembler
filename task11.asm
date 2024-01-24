d176ddb3383378b17703bae0dcb0d3faf6bf12e1
DamianPietron <128989743+neuropython@users.noreply.github.com>
2024-01-24 19:38:40 +0100

    Update task11.asm

--- task11.asm
+++ task11.asm
@@ -8,129 +8,149 @@
 
 ; Replace with your application code
 .equ stack = 0x0700
-.cseg 
-.org 0 
+.cseg
+.org 0
 rjmp start
 
-start: 
-	ldi r16, low(stack)
-	out SPL, r16 
-	ldi r16, high(stack)
-	out SPH, r16 
+start:
+ldi r16, low(stack)
+out SPL, r16
+ldi r16, high(stack)
+out SPH, r16
 
-	ldi r16, 0
-	out DDRB, r16
+ldi r16, 0
+out DDRB, r16
 
-	ldi r16, 0xff
-	out DDRC, r16
-	rjmp test 
+ldi r16, 0xff
+out DDRC, r16
+rjmp signals
 
-test:
-	.org 0x0200 
-	array: .db 100,0, 110,0, 120,0, 130,0, 140,0, 150,0, 160,0, 170,0, 200,0
-	ldi zl, low(array*2)
-	ldi zh, high(array*2)
+array: .db 95,0, 96,0, 105,0, 106,0, 115,0, 116,0, 125,0, 126,0
+;array: .db 135,0, 136,0, 145,0, 146,0, 165,0, 166,0, 185,0, 186,0
 
 
-.org 0x0400
+turn_off:
+ldi r17, 0b11111111
+out PORTC, r17
+
 Signals:
-	ldi r17, 0 
-	in r16, PINB
-	mov r18,r16
+ldi zl, low(array*2)
+ldi zh, high(array*2)
+ldi r17, 0
+in r16, PINB
+com r16
+brbs 1, turn_off
+mov r18,r16
 
-	calc_buttons:
-	inc r17
-	cpi r17, 9
-	brbs 1, Signals
-	lsr r18
-	brbs 0, loop
-	jmp calc_buttons
+calc_buttons:
+inc r17
+cpi r17, 9
+brbs 1, Signals
+lsr r18
+brbs 0, loop
+jmp calc_buttons
 
-	loop:
-		lsl r17
-		add r30, r17
-		lpm r16 , z
-		cpi r16, 255
-		in r0, sreg
-		sbrc r0, 1 
-		rjmp theend
-		call ligh_Signals
+loop:
+lsl r17
+subi r17,2
+add r30, r17
+lpm r16 , z
+cpi r16, 255
+in r0, sreg
+sbrc r0, 1
+rjmp theend
 
-theend: jmp theend
+call ligh_Signals
+jmp Signals
 
+theend:
+ldi r17, 0b11111100
+out PORTC, r17
+jmp theend
+
+.org 0x0200
 ligh_Signals:
-	cpi r16, 96
-	in r0, sreg
-	sbrc r0, 0 
-	rjmp message_1
 
-	cpi r16, 105
-	in r0, sreg
-	sbrc r0, 0 
-	rjmp message_2
+cpi r16, 96
+in r0, sreg
+sbrc r0, 0
+rjmp message_1
 
-	cpi r16, 115
-	in r0, sreg
-	sbrc r0, 0 
-	rjmp message_3
+cpi r16, 106
+in r0, sreg
+sbrc r0, 0
+rjmp message_2
 
-	cpi r16, 125
-	in r0, sreg
-	sbrc r0, 0 
-	rjmp message_4
+cpi r16, 116
+in r0, sreg
+sbrc r0, 0
+rjmp message_3
 
-	cpi r16, 135
-	in r0, sreg
-	sbrc r0, 0 
-	rjmp message_5
+cpi r16, 126
+in r0, sreg
+sbrc r0, 0
+rjmp message_4
 
-	cpi r16, 145
-	in r0, sreg
-	sbrc r0, 0 
-	rjmp message_6
+cpi r16, 136
+in r0, sreg
+sbrc r0, 0
+rjmp message_5
 
-	cpi r16, 165
-	in r0, sreg
-	sbrc r0, 0 
-	rjmp message_7
+cpi r16, 146
+in r0, sreg
+sbrc r0, 0
+rjmp message_6
 
-	cpi r16, 185
-	in r0, sreg
-	sbrc r0, 0 
-	rjmp message_8
+cpi r16, 166
+in r0, sreg
+sbrc r0, 0
+rjmp message_7
 
-	end:
-	ret
+cpi r16, 186
+in r0, sreg
+sbrc r0, 0
+rjmp message_8
 
-	message_1: 
-		ldi r17, 0b01111111
-		out PORTC, r17
-		rjmp loop
-	message_2:
-		ldi r17, 0b00111111
-		out PORTC, r17
-		rjmp loop
-	message_3:
-		ldi r17, 0b10011111
-		out PORTC, r17
-		rjmp loop
-	message_4:
-		ldi r17, 0b11001111
-		out PORTC, r17
-		rjmp loop
-	message_5:
-		ldi r17, 0b11100111 
-		out PORTC, r17
-		rjmp loop
-	message_6:
-		ldi r17, 0b11110011
-		out PORTC, r17
-		rjmp loop
-	message_7:
-		ldi r17, 0b11111001
-		out PORTC, r17
-		rjmp loop
-	message_8:
-		ldi r17, 0b11111100
-		out PORTC, r17
-		rjmp loop
+cpi r16, 255
+in r0, sreg
+sbrc r0, 0
+rjmp message_9
+end:
+ret
+
+message_1:
+ldi r17, 0b01111111
+out PORTC, r17
+rjmp end
+message_2:
+ldi r17, 0b00111111
+out PORTC, r17
+rjmp end
+message_3:
+ldi r17, 0b10011111
+out PORTC, r17
+rjmp end
+message_4:
+ldi r17, 0b11001111
+out PORTC, r17
+rjmp end
+message_5:
+ldi r17, 0b11100111
+out PORTC, r17
+rjmp end
+message_6:
+ldi r17, 0b11110011
+out PORTC, r17
+rjmp end
+message_7:
+ldi r17, 0b11111001
+out PORTC, r17
+rjmp end
+message_8:
+ldi r17, 0b11111100
+out PORTC, r17
+rjmp end
+message_9:
+ldi r17, 0b11111110
+out PORTC, r17
+rjmp end
